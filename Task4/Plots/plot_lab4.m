08ba2c95fa8f2c2768fb71359e92d12e416dac0a
Christian Le <Christian>
2023-04-19 13:28:52 +0200

    add temporary changes, unstable version

--- Task4/Plots/plot_lab4.m
+++ Task4/Plots/plot_lab4.m
@@ -22,6 +22,12 @@ pdot_traj = task4_traj(7,traj_init_points:traj_datapoints);
 e_traj = task4_traj(8,traj_init_points:traj_datapoints);
 edot_traj = task4_traj(9,traj_init_points:traj_datapoints);
 
+clf;
+figure(1);
+
+lambda_constraint = linspace(0,pi,61);
+e_constraint = 0.2*exp(-20*(lambda_constraint - 2*pi/3).^2);
+
 for i=1:num_of_runs
 
     task4_run = load("../Data/task4_" + i + ".mat").pc_ec_lambda_r_p_pdot_e_edot;
@@ -34,31 +40,77 @@ for i=1:num_of_runs
     pdot = task4_run(7,init_points:ratio:datapoints);
     e = task4_run(8,init_points:ratio:datapoints);
     edot = task4_run(9,init_points:ratio:datapoints);
+
+    if i == 1
+        subplot(3,1,1);
+        plot(t,rad2deg(lambda)); grid on; grid minor; hold on;
+        Legend1=Cell(num_of_runs+1,1);
+        Legend1{1}="Calculated trajectory";
+        Legend1{i+1}="Actual trajectory, Run " + i;
+        title("Travel");
+        
+        subplot(3,1,2);
+        plot(t_traj,rad2deg(e_traj)); grid on; grid minor; hold on;
+        Legend2=Cell(num_of_runs+1,1);
+        Legend2{1}="Calculated trajectory";
+        Legend2{i+1}="Actual trajectory, Run " + i;
+        title("Elevation");
+        
+        subplot(3,1,3);
+        plot(rad2deg(lambda_traj),rad2deg(e_traj)); grid on; grid minor; hold on;
+        plot(rad2deg(lambda_constraint),rad2deg(e_constraint));
+        Legend3=Cell(num_of_runs+2,1);
+        Legend3{1}="Calculated trajectory";
+        Legend3{2}="Constraint";
+        Legend3{i+2}="Actual trajectory, Run " + i;
+        title("Helicopter trajectory");
+    elseif i == num_of_runs
+        subplot(3,1,1);
+        plot(t,rad2deg(lambda)); grid on; grid minor; hold on;
+        Legend1{i+1}="Actual trajectory, Run " + i;
+        legend(Legend1);
+        title("Travel");
+        
+        subplot(3,1,2);
+        plot(t_traj,rad2deg(e_traj)); grid on; grid minor; hold on;
+        Legend2{i+1}="Actual trajectory, Run " + i;
+        legend(Legend2);
+        title("Elevation");
+        
+        subplot(3,1,3);
+        plot(rad2deg(lambda_traj),rad2deg(e_traj)); grid on; grid minor; hold on;
+        plot(rad2deg(lambda_constraint),rad2deg(e_constraint));
+        Legend3{i+2}="Actual trajectory, Run " + i;
+        legend(Legend3);
+        title("Helicopter trajectory");
+    else
+        subplot(3,1,1);
+        plot(t,rad2deg(lambda)); grid on; grid minor; hold on;
+        %plot(t_traj,rad2deg(lambda_traj));
+        legappend("Actual trajectory, Run " + i);
+        %legend("Actual trajectory, Run " + i, "Calculated trajectory")
+        %title("Travel");
+        
+        subplot(3,1,2);
+        plot(t,rad2deg(e)); grid on; grid minor; hold on;
+        %plot(t_traj,rad2deg(e_traj));
+        legappend("Actual trajectory, Run " + i);
+        %legend("Actual trajectory, Run " + i, "Calculated trajectory")
+        %title("Elevation");
+        
+        subplot(3,1,3);
+        plot(rad2deg(lambda),rad2deg(e)); grid on; grid minor; hold on;
+        %plot(rad2deg(lambda_traj),rad2deg(e_traj));
+        %plot(rad2deg(lambda_constraint),rad2deg(e_constraint));
+        Legend3{i+2}="Actual trajectory, Run " + i;
+        %legend("Actual trajectory, Run " + i, "Calculated trajectory", "Constraint");
+        %title("Helicopter trajectory");
+    end
+
     
-    lambda_constraint = linspace(0,pi,61);
-    e_constraint = 0.2*exp(-20*(lambda_constraint - 2*pi/3).^2);
     
     
-    figure(i);
-    clf;
-    subplot(3,1,1);
-    plot(t,rad2deg(lambda)); grid on; grid minor; hold on;
-    plot(t_traj,rad2deg(lambda_traj));
-    legend("Actual trajectory", "Calculated trajectory")
-    title("Travel");
     
-    subplot(3,1,2);
-    plot(t,rad2deg(e)); grid on; grid minor; hold on;
-    plot(t_traj,rad2deg(e_traj));
-    legend("Actual trajectory", "Calculated trajectory")
-    title("Elevation");
-    
-    subplot(3,1,3);
-    plot(rad2deg(lambda),rad2deg(e)); grid on; grid minor; hold on;
-    plot(rad2deg(lambda_traj),rad2deg(e_traj));
-    plot(rad2deg(lambda_constraint),rad2deg(e_constraint));
-    legend("Actual trajectory", "Calculated trajectory", "Constraint");
-    title("Helicopter trajectory");
     
     sgtitle("Run " + i);
 
